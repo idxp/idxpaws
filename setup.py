bb8f95d57b6ba1c274ede1d045bba6618ca4d338
Artur Rodrigues <arturhoo@gmail.com>
2015-05-28 14:39:12 -0300

    Include simple setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Idxpaws',
+    version='0.1.0',
+    author='Artur Rodrigues, Denis Lins',
+    author_email='arturhoo@gmail.com, denis.lins@outlook.com',
+    description='IDXP AWS - Simple libs for AWS services',
+    url='https://github.com/idxp/idxpaws',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'boto'
+    ],
+    tests_require=['pytest'],
+)
