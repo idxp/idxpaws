1c4706ccb04cde2086dcb422d0b0d7cff8003e5d
Artur Rodrigues <arturhoo@gmail.com>
2015-05-28 16:42:10 -0300

    Update version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyidxp',
-    version='0.1.1',
+    version='0.1.2',
     author='Artur Rodrigues, Denis Lins',
     author_email='arturhoo@gmail.com, denis.lins@outlook.com',
     description='pyidxp - simple libs for our everyday needs',
     url='https://github.com/idxp/pyidxp',
-    download_url='https://github.com/idxp/pyidxp/tarball/0.1.0',
+    download_url='https://github.com/idxp/pyidxp/tarball/0.1.2',
     license='LICENSE',
     packages=find_packages(exclude=['tests']),
     install_requires=[
