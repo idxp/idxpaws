eb8e9fdfa21f3b362667aa6ba154575345b2b0ce
Artur Rodrigues <arturhoo@gmail.com>
2015-05-28 14:58:46 -0300

    Add a setup.cfg

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author_email='arturhoo@gmail.com, denis.lins@outlook.com',
     description='IDXP AWS - Simple libs for AWS services',
     url='https://github.com/idxp/idxpaws',
+    download_url='https://github.com/idxp/idxpaws/tarball/0.1.0',
     license='LICENSE',
     packages=find_packages(exclude=['tests']),
     install_requires=[
