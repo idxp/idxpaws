b8654fac8ce282501b0830ba48b7e372d3c59230
Artur Rodrigues <arturhoo@gmail.com>
2015-05-28 17:51:29 -0300

    Update dependency

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyidxp',
-    version='0.2.0',
+    version='0.2.1',
     author='Artur Rodrigues, Denis Lins',
     author_email='arturhoo@gmail.com, denis.lins@outlook.com',
     description='pyidxp - simple libs for our everyday needs',
     url='https://github.com/idxp/pyidxp',
-    download_url='https://github.com/idxp/pyidxp/tarball/0.2.0',
+    download_url='https://github.com/idxp/pyidxp/tarball/0.2.1',
     license='LICENSE',
     packages=find_packages(exclude=['tests']),
     install_requires=[
-        'boto >= 2.36'
+        'boto >= 2.36',
+        'python-consul >= 0.3.18'
     ],
     tests_require=['pytest'],
 )
