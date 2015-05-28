fcc86eb8434d89c15bd688845595c5936bc3bf5c
Artur Rodrigues <arturhoo@gmail.com>
2015-05-28 15:31:32 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='Idxpaws',
-    version='0.1.0',
+    version='0.1.1',
     author='Artur Rodrigues, Denis Lins',
     author_email='arturhoo@gmail.com, denis.lins@outlook.com',
     description='IDXP AWS - Simple libs for AWS services',
     url='https://github.com/idxp/idxpaws',
-    download_url='https://github.com/idxp/idxpaws/tarball/0.1.0',
+    download_url='https://github.com/idxp/idxpaws/tarball/0.1.1',
     license='LICENSE',
     packages=find_packages(exclude=['tests']),
     install_requires=[
